de8434b4bfaccf48660d208f2953f670c7609295
Edemone <edem.agbenyo@gmail.com>
2019-10-25 19:22:09 +0000

    Edit README file and update index.rb file to pass stickler

--- index.rb
+++ index.rb
@@ -1,33 +1,36 @@
+# frozen_string_literal: true
+
 module Enumerable
 	def my_each
+		arr = to_a
 		self.to_enum unless block_given?
 		n=0
 		while self.length > n
-			yield(self[n])
+			yield(arr[n])
 			n += 1
 		end
 	end
 
 	def my_each_with_index
+		arr = to_a
 		return self.to_enum unless block_given?
-		n=0
+		n = 0
 		while self.length > n
-			yield(self[n], n)
+			yield(arr[n], n)
 			n += 1
 		end
 	end
 
 	def my_select
-		return self.to_enum  unless block_given?
-		res = []
-		self.my_each do |v|
-			res << v if yield(v) == true
+	  return self.to_enum unless block_given?
+	  res = []
+	  self.my_each do |v|
+		  res << v if yield(v) == true
 		end
 		res
 	end
 
 	def my_all?
-		n=0
 		check = true
 		if block_given?
 			self.my_each{ |i| check = false unless yield(i)}
@@ -38,39 +41,36 @@ module Enumerable
 	end
 
 	def my_any?
-		n=0
 		check = false
 		if block_given?
-			self.my_each{ |i| check=true if yield(i) }
+		  my_each{ |i| check = true if yield(i) }
 		else
-			self.my_any?{ |obj| obj }
+		  my_any?{ |obj| obj }
 		end
 		
 		check
 	end
 
 	def my_none?
-		n=0
 		check = true
 		if block_given? 
-			self.my_each{ |i| check=false if yield(i) }
+		  my_each{ |i| check = false if yield(i) }
 		else
-			self.my_none{ |obj| obj}
+		  my_none{ |obj| obj}
 		end
 		check
 	end
 
-	def my_count *arg
-		n = 0
+	def my_count (*arg)
 		count = 0
 		unless block_given?
 			if arg.empty?
-				self.my_count { |i| i }
+			  my_count { |i| i }
 			else
-				self.my_count { |i| arg[0] == i }
+			  my_count { |i| arg[0] == i }
 			end
 		end
-		self.my_each { |i| count += 1 if yield(i) }
+			my_each { |i| count += 1 if yield(i) }
 		count
 
 	end
@@ -79,13 +79,11 @@ module Enumerable
 
 		return arr.to_enum unless block_given?
 		result = []
-		n = 0
-		self.my_each { |i| result << proc.call(i) }
+		my_each { |i| result << proc.call(i) }
 		result
-
 	end
 
-	def my_inject init = nil
+	def my_inject(init = nil)
 		n = 0
 		acc = init ? init + arr[0] : arr[0]
 
@@ -94,14 +92,13 @@ module Enumerable
 				acc = yield(acc, arr[n+1])
 				n += 1
 			end
-		else
 		end
 		acc
 
 	end
 
 	def multiply_els(arr)
-		my_inject(arr){|i,j| i *j}
+		my_inject(arr){ |i,j| i *j }
 	end
 
 end
