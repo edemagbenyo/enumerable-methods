9b2c19620f08271e0b748aebca0569e79aea0013
Edemone <edem.agbenyo@gmail.com>
2019-10-25 18:37:19 +0000

    complete implementing the methods

--- index.rb
+++ index.rb
@@ -1,6 +1,6 @@
 module Enumerable
 	def my_each
-		raise "No block provided. Add a block" unless block_given?
+		self.to_enum unless block_given?
 		n=0
 		while self.length > n
 			yield(self[n])
@@ -30,11 +30,7 @@ module Enumerable
 		n=0
 		check = true
 		if block_given?
-			while self.length > n
-				check = false unless yield(self[n]) == true
-				n += 1
-			end
-
+			self.my_each{ |i| check = false unless yield(i)}
 		else
 			self.my_all?{ |obj| obj }
 		end
@@ -45,10 +41,7 @@ module Enumerable
 		n=0
 		check = false
 		if block_given?
-			while self.length > n
-				check = true if yield(self[n]) == true
-				n += 1
-			end
+			self.my_each{ |i| check=true if yield(i) }
 		else
 			self.my_any?{ |obj| obj }
 		end
@@ -60,12 +53,9 @@ module Enumerable
 		n=0
 		check = true
 		if block_given? 
-			while self.length > n
-				check = false if yield(self[n]) == true
-				n += 1
-			end
+			self.my_each{ |i| check=false if yield(i) }
 		else
-			self.my_none?
+			self.my_none{ |obj| obj}
 		end
 		check
 	end
@@ -75,53 +65,36 @@ module Enumerable
 		count = 0
 		unless block_given?
 			if arg.empty?
-				while self.length > n
-					count +=1
-					n += 1
-				end
+				self.my_count { |i| i }
 			else
-				while arr.length > n
-					count +=1 if check == arr[n]
-					n += 1
-				end
+				self.my_count { |i| arg[0] == i }
 			end
 		end
-
-		while arr.length > n
-			count += 1 if yield(arr[n]) == true
-			n += 1
-		end
+		self.my_each { |i| count += 1 if yield(i) }
 		count
 
 	end
 
-	def my_map(arr)
+	def my_map(&proc)
 
-		return arr unless block_given?
+		return arr.to_enum unless block_given?
 		result = []
 		n = 0
-		while arr.length > n
-			result << yield(arr[n])
-			n += 1
-		end
+		self.my_each { |i| result << proc.call(i) }
 		result
 
 	end
 
-	def my_inject (arr,init = nil)
+	def my_inject init = nil
 		n = 0
 		acc = init ? init + arr[0] : arr[0]
 
 		if block_given?
 			while arr.length-1 > n
-				p "#{acc}, #{arr[n+1]}"
 				acc = yield(acc, arr[n+1])
 				n += 1
 			end
 		else
-			# op = operation.to_s
-			# p op
-			# p 1.+ 2
 		end
 		acc
 
@@ -132,20 +105,3 @@ module Enumerable
 	end
 
 end
-
-class Test
-	include Enumerable
-end
-
-test = Test.new
-p test.my_select([1,34,5,65]){|i| i >= 5}
-p test.my_all?([2,34,5,65,nil])#{|i| i > 1}
-p test.my_any?([false,nil])#{|i| i > 1}
-p test.my_none?([false,nil,true])#{|i| i < 1}
-p test.my_count([1,34,5,65]){|i| i > 1}
-p test.my_map([12,32,13,43]){ |i| i*2}
-p test.my_inject([12,32,13,43],10){|n,m| n + m }
-p test.multiply_els([2,4,5])
-
-# p 1.+ 3
-# test.my_each_with_index([1,34,5,65]){|i,j| p "#{i} : #{j}"}
