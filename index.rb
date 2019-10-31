b2feb4eecd9138d1993a0440738886c9258d05a5
Edemone <edem.agbenyo@gmail.com>
2019-10-31 23:04:09 +0000

    Fix my_inject to apply binary op on elements in block

--- index.rb
+++ index.rb
@@ -123,10 +123,14 @@ module Enumerable
 
   def my_inject(*args)
     arr = to_a
-    acc = 0
+    # ac÷c = 0
+    acc = args[0].is_a?(Integer) ? args[0] : first
     if block_given?
-      acc = args[0] || 0
-      my_each_with_index { |_, i| acc = yield(acc, arr[i]) }
+      if args.empty?
+        my_each_with_index { |val, i| acc = yield(acc, val) if i.positive? }
+      else
+        my_each_with_index { |val, i| acc = yield(acc, val) }
+      end
     elsif args[0].is_a?(Integer) && args[1].is_a?(Symbol)
       acc = args[0]
       my_each_with_index { |_, i| acc = args[1].to_proc.call(acc, arr[i]) }
